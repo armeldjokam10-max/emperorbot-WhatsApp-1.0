8eafc899b0f7d72aa1aef01dab901ed1ca942180
armeldjokam10-max <armeldjokam10@gmail.com>
2026-08-06 23:43:04 +0100

    Create commandes.js

---
+++ commandes.js
@@ -0,0 +1,18 @@
+const jeux = () => `🎮 *CATEGORIE JEUX* 🎮
+!ping - Vitesse du bot`
+
+const groupe = () => `👥 *GESTION GROUPE* 👥
+!kick @ - Expulser
+!add 2376... - Ajouter`
+
+const admin = () => `⚙️ *ADMIN BOT* ⚙️
+!ban @ - Bannir
+!broadcast msg - Message à tous`
+
+const info = () => `ℹ️ *INFOS BOT* ℹ️
+🤖 Nom: EMPEROR BOT PRO
+👑 Owner: Armel
+⚡ Prefix:!
+!menu - Revenir au menu`
+
+module.exports = { jeux, groupe, admin, info };
